f689c7e1484a327606ce803564fa136daed83bfd
Markus Keil <markus.keil@ethereum.org>
2024-02-12 17:57:09 +0100

    disable endless loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
-
 set -e
 
-# Get custom delay, else use 3600 seconds
-DELAY="${DELAY:-3600}"
+echo "Starting to create mirrors..."
+node /app/src/index.js
 
-while true
-do
-  echo "Starting to create mirrors..."
-  node /app/src/index.js
-
-  echo "Waiting for ${DELAY} seconds..."
-  sleep "${DELAY}"
-done
